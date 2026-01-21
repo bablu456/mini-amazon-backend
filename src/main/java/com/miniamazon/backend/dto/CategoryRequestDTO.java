e0df6d6ceb4cccf781d591f50d2e3a8462fa0a34
bablu kumar <bablu707041dk@gmail.com>
2026-01-21 13:56:26 +0530

    Added The CategoryResponse And Category Request Dto

---
+++ src/main/java/com/miniamazon/backend/dto/CategoryRequestDTO.java
@@ -0,0 +1,9 @@
+package com.miniamazon.backend.dto;
+
+import lombok.Data;
+
+@Data
+public class CategoryRequestDTO {
+    private String name;
+    private String description;
+}
---
+++ src/main/java/com/miniamazon/backend/dto/CategoryResponseDTO.java
@@ -0,0 +1,10 @@
+package com.miniamazon.backend.dto;
+
+import lombok.Data;
+
+@Data
+public class CategoryResponseDTO {
+    private String id;
+    private String name;
+    private String description;
+}
