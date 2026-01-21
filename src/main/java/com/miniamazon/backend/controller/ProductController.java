c294407d845485181cee78eb840cc3b2c7d279d5
bablu kumar <bablu707041dk@gmail.com>
2026-01-21 22:26:47 +0530

    feat(product): add product controller with filtering endpoint
    - Implemented ProductController
    - Added POST /api/products (Create)
    - Added GET /api/products (List All)
    - Added GET /api/products/category/{categoryId} (Filter by Category)
    - Completed core backend modules (User, Category, Product)

---
+++ src/main/java/com/miniamazon/backend/controller/ProductController.java
@@ -0,0 +1,39 @@
+package com.miniamazon.backend.controller;
+
+import com.miniamazon.backend.dto.ProductRequestDTO;
+import com.miniamazon.backend.dto.ProductResponseDTO;
+import com.miniamazon.backend.model.Product;
+import com.miniamazon.backend.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<ProductResponseDTO> createProduct(@RequestBody ProductRequestDTO productRequestDTO){
+        ProductResponseDTO newProduct = productService.createProduct(productRequestDTO);
+        return new ResponseEntity<>(newProduct, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponseDTO>> getAllProducts() {
+        List<ProductResponseDTO> products = productService.getAllProducts();
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @GetMapping("/category/{categoryId")
+    public ResponseEntity<List<ProductResponseDTO>> getProductsByCategory(@PathVariable String categoryId){
+        List<ProductResponseDTO> products = productService.getProductsByCategoryId(categoryId);
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+}
