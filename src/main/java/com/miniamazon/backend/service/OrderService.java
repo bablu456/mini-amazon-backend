33d965c9e56709b1fb7aa397ee82dd24551598c3
bablu kumar <bablu707041dk@gmail.com>
2026-01-24 20:24:36 +0530

    feat: Create The OrderService

---
+++ src/main/java/com/miniamazon/backend/service/OrderService.java
@@ -0,0 +1,12 @@
+package com.miniamazon.backend.service;
+
+import com.miniamazon.backend.dto.OrderRequestDTO;
+import com.miniamazon.backend.dto.OrderResponseDTO;
+
+import java.util.List;
+
+public interface OrderService {
+    OrderResponseDTO placeOrder(OrderRequestDTO orderRequestDTO);
+
+    List<OrderResponseDTO> getOrdersByUsersId(String userId);
+}
